18537a1866978ecd7db328c1baf1261208a9a6a0
Egi Yuda Sebayang <egiyudasebayang@gmail.com>
2026-03-31 16:19:19 +0700

    membuat kerangka kerja tiap anggota

--- main.c
+++ main.c
@@ -1,7 +1,163 @@
 #include <stdio.h>
 
+// deklarasi fungsi per fitur
+void diskon();        // tugas yuki
+void splitBill();    // tugas naura
+void bunga();         // tugas rafael
+void sisaUang();     // tugas tika
+
+// fungsi main dan tampilan -> tugas Egi dan Hilal
 int main(){
+
+     // tempat kerja hilal - membuat tampilan UI
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+     //Tugas EGI
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
     return 0;
+}
+
+
+// ================== FITUR 1 ============================
+// tugas yuki
+void diskon() {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+// ================== FITUR 2 ============================
+// tugas naura
+void split_bill() {
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+}
+
+// ================== FITUR 3 ============================
+// tugas rafael
+void bunga() {
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+// ================== FITUR 4 ============================
+// tugas tika
+void sisa_uang() {
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
 }
