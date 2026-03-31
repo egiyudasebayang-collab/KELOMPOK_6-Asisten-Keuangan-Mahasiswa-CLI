f523ed18139674293376a91669eed5f99ee15f2e
Rafael Haidar Arash <raphaelkingstar@gmail.com>
2026-03-31 19:36:32 +0700

    final Kakulator bunga penambahan hasil dan penjumlahan

--- main.c
+++ main.c
@@ -122,7 +122,27 @@ void split_bill() {
 // ================== FITUR 3 ============================
 // tugas rafael
 void bunga() {
-        
+         double saldo_awal, bunga_bulanan, total_akhir;
+
+    printf("   KALKULATOR BUNGA TABUNGAN SEDERHANA  \n");
+
+    printf("Masukkan saldo awal (Rp): ");
+    scanf("%lf", &saldo_awal);
+
+    printf("Masukkan persentase bunga bulanan (%%): ");
+    scanf("%lf", &bunga_bulanan);
+    double bunga = saldo_awal * (bunga_bulanan / 100);
+    total_akhir = saldo_awal + bunga;
+
+    printf("        Hasil        ");
+    printf("saldo awal : Rp %.2f\n", saldo_awal);
+    printf("bunga (%.2f%%) : Rp %.2f\n", bunga_bulanan, bunga);
+    printf("total akhir : Rp %.2f\n", total_akhir);
+}
+int main() {
+    bunga();
+    return 0;
+}")
 
 
 
